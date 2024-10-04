1083056d37a7a735553bea245215ff39e31b0c79
Todd H <github@simplybest.com>
2024-10-04 10:15:52 -0600

    update name and id for apps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'janTabs',
+  appId: 'edu.mtec.thale.jantabs',
+  appName: 'Awesomeness',
   webDir: 'www'
 };
 
