74592920406ccd3d8e5fdc37cdcf6fe4e44c7ab1
Todd H <github@simplybest.com>
2024-10-06 21:22:19 -0600

    add routerlink button

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,18 +5,18 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 import { addIcons } from 'ionicons';
 import { settingsOutline } from 'ionicons/icons';
 import { SettingsPage } from '../settings/settings.page';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonTabButton, IonCheckbox, IonInput, IonIcon, IonButtons, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [RouterModule, IonTabButton, IonCheckbox, IonInput, IonIcon, IonButtons, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
 
-  private router: Router = inject(Router);
+  private readonly router: Router = inject(Router);
 
   constructor(private readonly modalCtrl: ModalController) {
     addIcons({
